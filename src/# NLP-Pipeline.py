71ffa92aeda5aec25a2e7b178e8ea249fc8365e2
IU-KaiW <kai.weber@iu-study.org>
2026-01-19 13:49:02 +0100

    Update README.md

---
+++ src/# NLP-Pipeline.py
@@ -0,0 +1,27 @@
+# NLP-Pipeline
+import nltk 
+import sklearn
+import gensim
+# import spaCy as sp
+import re
+
+# ML
+import transformers
+import torch 
+
+## Embeddings
+
+import top2vec as t2v 
+
+## Visualisierung
+import matplotlib as mpl
+%matplotlib inline
+import matplotlib.pyplot as plt
+import seaborn as sns
+import wordcloud as wc
+import pyLDAvis
+import cartopy as ctp
+
+# Sortieren
+import venv
+import numpy as np
